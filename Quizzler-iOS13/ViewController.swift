eab18cfe55acbde2f07cc46c1bf2edb75eea3cdc
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-06 23:00:26 +0300

    Completed second Challenge of making label showing the first item out of array of the Quiz

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,24 +15,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    /*
     let question = (text: "Four + Two is equal to Six",
                     answer: "True")
+     */
+    
+    let quiz = [
+        "Four + Two is equal to Six",
+        "Five - Three is greater than One",
+        "Three + Two is less than Ten"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        questionLabel.text = question.text
+//        questionLabel.text = question.text
+        questionLabel.text = quiz.first
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        /*
         if let answer = sender.currentTitle {
-            
             if answer == question.answer {
                 questionLabel.text = question.answer
             } else {
                 questionLabel.text = "Mistaken"
             }
         }
+        */
     }
 }
 
